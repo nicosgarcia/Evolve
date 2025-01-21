e15d9654d2465ee8c5464546ee839364ea8ef57a
Nicolas Sousa <nicolassousa.oficial@gmail.com>
2025-01-21 20:22:49 -0300

    first commit

---
+++ game/game.js
@@ -0,0 +1,13 @@
+let contador = 0;
+let specialButton = document.getElementById("primeiraEv");
+let pontosParaAparecer = 10
+
+function coletar() {
+    contador++;
+
+    document.getElementById("score").innerText = contador;
+
+}
+
+
+
