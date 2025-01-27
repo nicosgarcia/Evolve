61186c13ff0590b380d6a5ece86d080e8ba3ce9a
Nicolas Sousa <nicolassousa.oficial@gmail.com>
2025-01-27 18:43:58 -0300

    Added more features

--- game/game.js
+++ game/game.js
@@ -1,4 +1,5 @@
 // VARIAVEIS
+let contagemContador = 1;
 let contador = 0;
 let multiplicador = 0;
 let transcender = 1;
@@ -12,9 +13,12 @@ const coletarSoundEffect = document.getElementById("coletarSound");
 const evSoundAudio = document.getElementById("gameMusic");
 
 // BOTÕES
+const specialButtonWait = document.getElementById("evolve");
 const specialButton = document.getElementById("primeiroUp");
 const specialButtonTranscend = document.getElementById("segundoUp");
 const specialButtonEvolution = document.getElementById("primeiraEv");
+const specialButtonPrimeiroEst = document.getElementById("primeiroEst");
+const specialButtonFeed = document.getElementById("feed");
 
 const menu = document.getElementById("menu");
 const game = document.getElementById("game");
@@ -22,8 +26,14 @@ const game = document.getElementById("game");
 // PONTOS PARA APARECER OS BOTÕES
 let pontosParaAparecer = 10;
 let pontosEvolution = 50;
+let pontosPrimeiroEst = 100;
 let pontosTranscend = 1000;
 
+// FEED
+
+let feedCheck = 0;
+let FEED = 150;
+let feedCount = 0;
 
 // CÓDIGO
 
@@ -56,113 +66,206 @@ function checarAparicao(animacao, pontos) {
     if (contador >= pontos) {
         document.getElementById(animacao).style.visibility = "visible";
     }
+
+    contador = contador - pontos;
+    atualizarScore();
 }
 
 function atualizarScore() {
   document.getElementById("score").innerText = contador;
 }
 
+function checarCompra() {
+
+  if (contador < pontosParaAparecer) {
+    specialButton.style.visibility = "hidden";
+    specialButton.style.pointerEvents = "none";
+  }
+
+  if (contador < pontosTranscend) {
+    specialButtonTranscend.style.visibility = "hidden";
+    specialButtonTranscend.style.pointerEvents = "none";
+  }
+
+  if (contador < pontosEvolution) {
+    specialButtonEvolution.style.visibility = "hidden";
+    specialButtonEvolution.style.pointerEvents = "none";
+  }
+
+  if (contador < pontosPrimeiroEst) {
+    specialButtonPrimeiroEst.style.visibility = "hidden";
+    specialButtonPrimeiroEst.style.pointerEvents = "none";
+  }
+
+  if (contador < FEED) {
+    specialButtonFeed.style.visibility = "hidden";
+    specialButtonFeed.style.pointerEvents = "none";
+  }
+}
 
 function coletar() {
-  coletarSoundEffect.currentTime = 0;
-  coletarSoundEffect.play();
+    coletarSoundEffect.currentTime = 0;
+    coletarSoundEffect.play();
 
-  contador = contador + 1 + (multiplicador * transcender);
-  atualizarScore()
+    contador = contador + 1 + (multiplicador * transcender);
+    atualizarScore()
 
-  if (contador >= pontosParaAparecer) {
-    specialButton.style.visibility = "visible";
-    specialButton.style.pointerEvents = "auto";
+    const divContainer = document.getElementById("container")
+    const texto = document.createElement("div");
+    texto.classList.add("pontos");
+    texto.textContent = contagemContador;
+
+    divContainer.appendChild(texto);
+
+    setTimeout(() => {
+        texto.remove();
+    }, 1000);
+
+    if (contador >= pontosParaAparecer) {
+        specialButton.style.visibility = "visible";
+        specialButton.style.pointerEvents = "auto";
+    }
+
+    if (contador >= pontosEvolution) {
+        specialButtonEvolution.style.visibility = "visible";
+        specialButtonEvolution.style.pointerEvents = "auto";
+    }
+
+    if (contador >= pontosTranscend) {
+        specialButtonTranscend.style.visibility = "visible";
+        specialButtonTranscend.style.pointerEvents = "auto";
+    }
+
+    if (contador >= pontosPrimeiroEst) {
+        specialButtonPrimeiroEst.style.visibility = "visible";
+        specialButtonPrimeiroEst.style.pointerEvents = "auto";
+    }
+
+    if (contador >= FEED && feedCheck == 1) {
+        specialButtonFeed.style.visibility = "visible";
+        specialButtonFeed.style.pointerEvents = "auto";
   }
-
-  if (contador >= pontosEvolution) {
-    specialButtonEvolution.style.visibility = "visible";
-    specialButtonEvolution.style.pointerEvents = "auto";
-  }
-
-  if (contador >= pontosTranscend) {
-    specialButtonTranscend.style.visibility = "visible";
-    specialButtonTranscend.style.pointerEvents = "auto";
-  }
-
-  checarAparicao("egg-moving", 50)
 }
 
 function primeiroUp() {
 
-  audio.currentTime = 0;
-  audio.play();
+    audio.currentTime = 0;
+    audio.play();
 
-  multiplicador++;
-  contador = contador - pontosParaAparecer;
-  atualizarScore()
+    multiplicador++;
+    contador = contador - pontosParaAparecer;
+    contagemContador++;
+    atualizarScore()
 
-  pontosParaAparecer = Math.ceil(pontosParaAparecer * 1.5);
-  specialButton.style.visibility = "hidden";
-  specialButton.style.pointerEvents = "none";
+    pontosParaAparecer = Math.ceil(pontosParaAparecer * 1.5);
+    specialButton.style.visibility = "hidden";
+    specialButton.style.pointerEvents = "none";
 
-  if (contador < pontosTranscend) {
-    specialButtonTranscend.style.visibility = "hidden";
-    specialButtonTranscend.style.pointerEvents = "none";
-  }
+    checarCompra();
 
-  if (contador < pontosEvolution) {
-    specialButtonEvolution.style.visibility = "hidden";
-    specialButtonEvolution.style.pointerEvents = "none";
-  }
 }
 
 function segundoUp() {
 
-  audio.currentTime = 0;
-  audio.play();
+    audio.currentTime = 0;
+    audio.play();
 
-  transcender = transcender + 2;
-  contador = contador - Math.floor(pontosTranscend / 2);
-  atualizarScore()
+    transcender = transcender + 2;
+    contador = contador - Math.floor(pontosTranscend / 2);
+    contagemContador = contagemContador * 2;
+    atualizarScore()
 
-  pontosTranscend = Math.ceil(pontosTranscend * 1.5);
-  specialButtonTranscend.style.visibility = "hidden";
-  specialButtonTranscend.style.pointerEvents = "none";
+    pontosTranscend = Math.ceil(pontosTranscend * 1.5);
+    specialButtonTranscend.style.visibility = "hidden";
+    specialButtonTranscend.style.pointerEvents = "none";
 
-  if (contador < pontosParaAparecer) {
-    specialButton.style.visibility = "hidden";
-    specialButton.style.pointerEvents = "none";
-  }
+    checarCompra()
 
-  if (contador < pontosEvolution) {
-    specialButtonEvolution.style.visibility = "hidden";
-    specialButtonEvolution.style.pointerEvents = "none";
-  }
 }
 
 function primeiraEv() {
 
-  audio.currentTime = 0;
-  audio.play();
+    audio.currentTime = 0;
+    audio.play();
 
-  acrescentador++;
-  contador = contador - Math.floor(pontosEvolution / 2);
-  atualizarScore()
-  pontosEvolution = Math.ceil(pontosEvolution * 1.75);
+    acrescentador++;
+    contador = contador - Math.floor(pontosEvolution / 2);
+    atualizarScore()
+    pontosEvolution = Math.ceil(pontosEvolution * 1.75);
 
-  if (intervalo === null) {
+    if (intervalo === null) {
     intervalo = setInterval(() => {
       contador = contador + acrescentador;
       document.getElementById("score").innerText = contador;
     }, 1000);
-  }
+    }
 
-  specialButtonEvolution.style.visibility = "hidden";
-  specialButtonEvolution.style.pointerEvents = "none";
+    specialButtonEvolution.style.visibility = "hidden";
+    specialButtonEvolution.style.pointerEvents = "none";
 
-  if (contador < pontosParaAparecer) {
-    specialButton.style.visibility = "hidden";
-    specialButton.style.pointerEvents = "none";
-  }
+    checarCompra();
+}
 
-  if (contador < pontosTranscend) {
-    specialButtonTranscend.style.visibility = "hidden";
-    specialButtonTranscend.style.pointerEvents = "none";
-  }
+
+function primeiroEst() {
+
+    audio.currentTime = 0;
+    audio.play();
+
+    checarAparicao("egg-moving", 100)
+
+    specialButtonPrimeiroEst.style.visibility = "hidden";
+    specialButtonPrimeiroEst.style.pointerEvents = "none";
+
+    pontosPrimeiroEst = NaN;
+    feedCheck = 1;
+
+    checarCompra();
+}
+
+function feed() {
+
+    audio.currentTime = 0;
+    audio.play();
+
+    feedCount++;
+
+    switch(true) {
+        case feedCount == 0:
+            contador = contador - FEED;
+            atualizarScore();
+            FEED = FEED * 2;
+
+
+            specialButtonFeed.style.visibility = "hidden";
+            specialButtonFeed.style.pointerEvents = "none";
+
+            checarCompra();
+
+            break;
+
+        case feedCount == 1:
+            contador = contador - FEED;
+            atualizarScore();
+            FEED = FEED * 2;
+
+            specialButtonFeed.style.visibility = "hidden";
+            specialButtonFeed.style.pointerEvents = "none";
+
+            checarCompra();
+
+            break;
+
+        case feedCount == 2:
+            contador = contador - FEED;
+            atualizarScore();
+            FEED = FEED * 2;
+
+            specialButtonFeed.style.visibility = "visible";
+            specialButtonFeed.style.pointerEvents = "none";
+
+            checarCompra();
+
+            break;
+    }
 }
